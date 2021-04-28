32c971adbed064bcb04c3482f92cc9a6e6f8a817
Muhammad Zahid <muhammadali775@gmail.com>
2021-04-28 15:43:26 -0400

    commit tree e0f7e1d416248b036c837082b7500d61c17169d8

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+  function returnFirstTwoDrivers(array){
+const newArray = [];
+ newArray[0] = array[0]
+ newArray[1] = array[1]
+
+ return newArray;
+  }
+
+  function returnLastTwoDrivers(array){
+    const lastTwodrivers = array.slice(array.length-2)
+     
+     return lastTwodrivers
+      }
+
+  const selectingDrivers = []
+
+      selectingDrivers.push(returnFirstTwoDrivers, returnLastTwoDrivers)
+
+  
+      function createFareMultiplier(Multiplier)  {
+         const calcFare = (fare) => fare*Multiplier
+         return calcFare
+
+      }
+
+   function fareDoubler(fare) {
+          
+    return createFareMultiplier(2)(fare);
+
+                 
+      }
+
+      function fareTripler(fare) {
+
+    return createFareMultiplier(3)(fare)
+
+      }
+
+    function selectDifferentDrivers(array, x) {
+
+
+          return x(array);
+        }
+
